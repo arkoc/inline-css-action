5bb67a7ea3d3fd4b5f20166be57bebfb52f5d6f7
arkoc <aram.koch@gmail.com>
2022-04-29 21:28:39 +0400

    Update resolve path

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { promises: fs } = require('fs')
 const inlineCss = require('inline-css');
+const path = require('path');
 
 (async function()
 {
     try {
         const html_file_path = core.getInput('html-path');
         let content = await fs.readFile(html_file_path, 'utf8');
-        const html = await inlineCss(content, {url: "/"});
+        
+        const html = await inlineCss(content, {url: `file://${path.resolve(html_file_path)}`});
         let result = await fs.writeFile(html_file_path, html);
       } catch (error) {
         core.setFailed(error);
