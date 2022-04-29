388234b5997729493114b27f817d2fbfe83c85cc
arkoc <aram.koch@gmail.com>
2022-04-29 21:05:45 +0400

    Update overwrite files

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const github = require('@actions/github');
 const { promises: fs } = require('fs')
 const inlineCss = require('inline-css');
 
-
-
 (async function()
 {
     try {
         const html_file_path = core.getInput('html-path');
-        let content = await fs.readFile(html_file_path, 'utf8')
-        const html = await inlineCss(content, {url: "/"})
-        core.setOutput("content", html);
+        let content = await fs.readFile(html_file_path, 'utf8');
+        const html = await inlineCss(content, {url: "/"});
+        let result = await fs.writeFile(html_file_path, html);
       } catch (error) {
         core.setFailed(error);
       }
