b5d8ceefa319a4bf5dbdbf7a1cabce1318379f84
arkoc <aram.koch@gmail.com>
2022-04-29 21:00:40 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { promises: fs } = require('fs')
+const inlineCss = require('inline-css');
+
+
+
+(async function()
+{
+    try {
+        const html_file_path = core.getInput('html-path');
+        let content = await fs.readFile(html_file_path, 'utf8')
+        const html = await inlineCss(content, {url: "/"})
+        core.setOutput("content", html);
+      } catch (error) {
+        core.setFailed(error);
+      }
+})();
